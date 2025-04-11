daa294ab6749553a0468ea1c6e8b4898e4778466
Carlos Garijo <100538601@alumnos.uc3m.es>
2025-04-11 21:33:05 +0200

    Initialize app + cuda

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 import subprocess
 import requests
+import time
 
 from src.chatbot import ChatBot
 from src.db import VectorDB
 
-def main():
+def initialize_app():
     llm_exposed_port = 5001
 
     # This just checks if the LLM docker is already loaded, if not, it launches it.
@@ -14,12 +15,20 @@ def main():
     except requests.ConnectionError:
         # If this exception happens, the LLM service app hasn't been launched.
         subprocess.run("docker compose up --build -d", shell=True, stdout=subprocess.DEVNULL)
+        time.sleep(2)
+        response = requests.get(f"http://localhost:{llm_exposed_port}/health")
+        response.raise_for_status()
 
     if response.status_code != 200:
-        print(response)
+        # If status code is not 200 something is wrong.
+        raise response
     else:
+        print(response)
         print("Model is already loaded.")
 
+
+
+def main():
     chatbot = ChatBot()
     vector_db = VectorDB()
 
@@ -31,4 +40,5 @@ def main():
 
 
 if __name__ == "__main__":
+    initialize_app()
     main()
